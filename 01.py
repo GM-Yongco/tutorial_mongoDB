848ee248e661f1f8ded4aef65587e8b6bb628004
Giulio Yongco <yongcogiulio125@gmail.com>
2024-06-28 18:58:46 +0800

    First query
    :DDD

--- 01.py
+++ 01.py
@@ -2,20 +2,25 @@
 # Date				: ur my date uwu
 # Description		: Code that will impress u ;)
 # HEADERS ================================================================
+import json
+
+from pymongo import MongoClient, database, collection
 
 # ========================================================================
 # FUNCTIONS MISC
 # ========================================================================
 
 def separator(x:str = "SECTION") -> None:
-	ret_val = f"\n {x} {'-' * (40 - len(x))}\n"
-	print(ret_val)
+	section:str = f"\n {x} {'-' * (40 - len(x))}\n"
+	print(section)
 
 # ========================================================================
 # FUNCTIONS 
 # ========================================================================
 
-
+def get_credentials()->json:
+	file_path:str = "REFERENCES/auth.json"
+	return json.load(open(file_path))
 
 # ========================================================================
 # MAIN 
@@ -23,4 +28,22 @@ def separator(x:str = "SECTION") -> None:
 
 if __name__ == '__main__':
 	separator("START")
+
+	user_credentials:json = get_credentials()
+	driver_connection:str = f"mongodb+srv://{user_credentials['username']}:{user_credentials['password']}@cluster-01.f4zbhsn.mongodb.net/?retryWrites=true&w=majority&appName=cluster-01"
+	
+	cluster:MongoClient = MongoClient(driver_connection)
+	db: database.Database = cluster["sample_analytics"]
+	col: collection.Collection = db["accounts"]
+	
+	for x in col.find() :
+		print(x)
+
+	separator("TYPES")
+
+	print(type(db))
+	print(type(col))
+
+	print(driver_connection)
+
 	separator("END")
