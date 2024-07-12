1bc201c799ed16b4055e4153c94876575f8ccbc9
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-12 19:36:03 +0800

    reformats

--- 02_find.py
+++ 02_find.py
@@ -38,11 +38,17 @@ if __name__ == '__main__':
 	db: database.Database = cluster["sample_analytics"]
 	coll: collection.Collection = db["accounts"]
 	
-	# how to query
+	# ====================================================================
 
-	query:json = {"limit":9000}
-	selection:json = { "limit": 1}
-	result:cursor.Cursor = coll.find(query, selection)
+	# documents with limits equal to 9000
+	# show id:false, account_id:true, limit: true
+	#s ort by account id in descending order
+
+	query_filter:json = {"limit":9000}	
+	query_projection:json = {"_id" : 0, "account_id": 1, "limit": 1}	
+	query_sort:json = {"account_id" : -1}
+
+	result:cursor.Cursor = coll.find(query_filter, projection = query_projection, sort = query_sort)
 
 	separator("RESULTS")
 
