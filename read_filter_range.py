9704835962ad2879c8b2e60f753ab4258fd635d9
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-28 20:32:17 +0800

    read with a ranged filter
    note: use regex notation nexttime

---
+++ read_filter_range.py
@@ -0,0 +1,37 @@
+# Author			: G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+# Date				: ur my date uwu
+# Description		: Code that will impress u ;)
+# HEADERS ================================================================
+
+from mongoDB_template import *
+
+# ========================================================================
+# MAIN 
+# ========================================================================
+
+if __name__ == '__main__':
+	separator("START")
+
+	coll : collection.Collection = get_collection(database_name = "friends", collection_name = "birthdays")
+	
+	# ====================================================================
+
+	query_filter:json = {"birth_date.year" : {"$gt": 2000, "$lt": 2003} }
+	query_projection:json = {"_id" : 0, 
+		"name":{
+			"name_first":1
+		}, "birth_date": {
+			"year": 1
+		}
+	}	
+	query_sort:json = {"account_id" : -1}
+
+	result:cursor.Cursor = coll.find(query_filter, projection = query_projection, sort = query_sort)
+
+	separator("RESULTS")
+
+	# print(f"number of results: {len(result)}")
+	for x in result:
+		print(x)
+
+	separator("END")
